036e32741c2f496f01bc8aedf1cadf7f23d12861
Stage18 <stage18@localhost.localdomain>
2019-08-02 12:33:50 +0200

    bon ap

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "A taaaaaaaaaaable !!!!!!!!!");
+	System.out.println( "Hello World!" );
   	System.out.println( "bonjour les petits loulous ! " );
         System.out.println("Hello from the prophet !");
     }
