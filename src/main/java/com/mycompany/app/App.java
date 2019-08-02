0d7385224e038f0c054f8226689fca6b0acc1adf
EC2 Default User <ec2-user@ip-172-31-27-224.eu-west-1.compute.internal>
2019-08-02 10:23:10 +0000

    dorsaf a buggue

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "salut Antoine , la cave est bien remplie ?
+        System.out.println( "salut Antoine , la cave est bien remplie ?");
 	System.out.println( "Je vais acheter le hoodie Moira dans la journée");
   	System.out.println( "bonjour les petits loulous ! " );
         System.out.println("Hello from the prophet !");
