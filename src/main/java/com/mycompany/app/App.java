75edd057b00682eb751c26389927e600f5c10eb7
Largeaud Alexandre <lalexandre@localhost.localdomain>
2019-08-02 12:03:14 +0200

    Une phrase en plus

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	System.out.println( "Je vais acheter le hoodie Moira dans la journée");
     }
 }
